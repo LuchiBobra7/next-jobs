acff0e3d6dfc9214bf139c29d40393cef041411c
Luchi Bobra <badger@example.com>
2021-11-01 18:38:29 +0200

    feat: job cards added

---
+++ components/job/card/index.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react'
+import React from 'react'
+import { Box, useColorModeValue } from '@chakra-ui/react'
+
+type Props = {
+  onClick: () => void
+}
+const JobCard: FC<Props> = ({ children, ...props }) => {
+  const bg = useColorModeValue('white', 'darkHighlight')
+  const boxShadow = useColorModeValue('md', 'none')
+  return (
+    <Box
+      as="article"
+      display="flex"
+      p={6}
+      bg={bg}
+      boxShadow={boxShadow}
+      borderLeftColor="brand.300"
+      transition="all .1s"
+      cursor="pointer"
+      _hover={{ boxShadow: 'lg' }}
+      {...props}
+    >
+      {children}
+    </Box>
+  )
+}
+
+export default JobCard
