0c27fa615c911e6e91be6b31548dfa7f637f61ff
Luchi Bobra <badger@example.com>
2021-11-03 14:12:35 +0200

    fix: broken image for jobs page

--- components/job/image/index.tsx
+++ components/job/image/index.tsx
@@ -31,13 +31,15 @@ const JobImage = ({
         loading="lazy"
         position="relative"
         lineHeight={2}
+        overflow="hidden"
         //Fallback for broken image
         _before={{
           content: '""',
           position: 'absolute',
           width: '100%',
           height: '100%',
-          background: `url(${fallbackSrc})`,
+          background: `url(${fallbackSrc}) no-repeat`,
+          backgroundSize: 'cover',
           left: '0px',
           display: 'flex',
         }}
